0f8c48f5c1812940d3ff41d4c731c088ceb4f03e
Avi Fisher <avifisher82@gmail.com>
2025-10-30 16:43:00 +0200

    creat "input_number" function

---
+++ ttt/rules.py
@@ -0,0 +1,37 @@
+def input_number(print):
+    while True
+        try:
+            num = int(input(f"enter your {print} number"))
+        except:
+            print("error peek only number")
+            continue
+
+        if num >= 1 and num <= 3:
+            return num
+
+def round_play(table):
+
+    row = input_number("row")
+    column = input_number("column")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
