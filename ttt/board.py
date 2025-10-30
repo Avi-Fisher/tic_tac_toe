b60b8105dfeeb121b3fe4b6036652419c2a6f1f4
Avi Fisher <avifisher82@gmail.com>
2025-10-30 16:04:12 +0200

    creat "creat_function" function

---
+++ ttt/board.py
@@ -0,0 +1,46 @@
+def creat_board():
+
+    row = []
+    for i in range(3):
+        column = []
+        for j in range(3):
+            column.append("$")
+        row.append(column)
+    return row
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
